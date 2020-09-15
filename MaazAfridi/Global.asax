29837301972147bcc249b56ed4ed83a11833dbb2
Maazafridi <maazafridi520@gmail.com>
2020-09-15 10:29:59 +0500

    first commit

---
+++ MaazAfridi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaazAfridi.MvcApplication" Language="C#" %>
---
+++ MaazAfridi/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MaazAfridi.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string publisher { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
